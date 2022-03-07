95d6fcfbbb2add612f0cd04a731e3929f1547ebb
honglin <1300170430@qq.com>
2022-03-07 22:16:21 +0800

    2022/03/07 commit

--- src/题目/Sum.java
+++ src/题目/Sum.java
@@ -3,7 +3,7 @@ package 题目;
 public class Sum {
     public static void main(String[] args) {
         int a = 3;
-        int b =5;
+        int b = 6;
         int sum = a + b;
         System.out.println(sum);
     }
