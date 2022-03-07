42c8c7290406d51873ebec590c9363553ca880e4
honglin <1300170430@qq.com>
2022-03-07 21:52:18 +0800

    2022/03/07 commit

---
+++ src/题目/Sum.java
@@ -0,0 +1,10 @@
+package 题目;
+
+public class Sum {
+    public static void main(String[] args) {
+        int a = 3;
+        int b =4;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+}
