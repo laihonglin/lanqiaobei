56916db34337c1d1e3ad06f7d7855ec0ea442726
Peter-bot-web <84163976+Peter-bot-web@users.noreply.github.com>
2022-03-09 21:25:33 +0800

    Update Sum.java

--- src/题目/Sum.java
+++ src/题目/Sum.java
@@ -2,7 +2,7 @@ package 题目;
 
 public class Sum {
     public static void main(String[] args) {
-        int a = 3;
+        int a = 4;
         int b = 6;
         int sum = a + b;
         System.out.println(sum);
