12186912ae3182d4b27aa4aa801609cfde494192
honglin <1300170430@qq.com>
2022-03-12 10:17:22 +0800

    2022/03/12 commit

---
+++ src/题目/DifferentString.java
@@ -0,0 +1,26 @@
+package 题目;
+
+public class DifferentString {
+    public static boolean checkDifferent(String initString) {
+        if (initString == null) {
+            return true;
+        }
+
+        int[] flag = new int[128];
+        for (int i = 0; i < initString.length(); i++) {
+            int c = (int) initString.charAt(i);
+            if (flag[c] > 0) {
+                return false;
+            } else {
+                flag[c]++;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String initString = "abcdefga";
+        boolean flag = checkDifferent(initString);
+        System.out.println(new DifferentString().checkDifferent(initString));
+    }
+}
