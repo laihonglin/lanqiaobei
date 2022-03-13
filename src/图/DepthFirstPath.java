266f98b0a95d620a6b5681280dd0c0f92b06b396
honglin <1300170430@qq.com>
2022-03-13 13:04:50 +0800

    2022/03/13 Commit

---
+++ src/图/DepthFirstPath.java
@@ -0,0 +1,40 @@
+package 图;
+
+import edu.princeton.cs.algs4.Stack;
+
+public class DepthFirstPath {
+    private boolean[] marked; // 这个顶点上调用过dfs()了吗？
+    private int[] edgeTo; // 从起点到一个顶点到已知路径上到最后一个顶点
+    private final int s; // 起点
+
+    public DepthFirstPath(Graph G, int s) {
+        marked = new boolean[G.V()];
+        edgeTo = new int[G.V()];
+        this.s = s;
+        dfs(G, s);
+    }
+
+    private void dfs(Graph G, int v) {
+        marked[v] = true;
+        for (int w : G.adj(v)) {
+            if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(G, w);
+            }
+        }
+    }
+
+    public boolean hasPathTo(int v) {
+        return marked[v];
+    }
+
+    public Iterable<Integer> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        Stack<Integer> path = new Stack<Integer>();
+        for (int x = v;x != s;x = edgeTo[x]){
+            path.push(x);
+        }
+        path.push(s);
+        return path;
+    }
+}
