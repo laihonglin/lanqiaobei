b26c5558b64238e3d2aabc2c8e58eb419090a44c
honglin <1300170430@qq.com>
2022-03-24 09:12:57 +0800

    2022/03/24 Update Data

--- src/题目/Sum.java
+++ src/题目/Sum.java
@@ -2,7 +2,7 @@ package 题目;
 
 public class Sum {
     public static void main(String[] args) {
-        int a = 5;
+        int a = 6;
         int b = 6;
         int sum = a + b;
         System.out.println(sum);
