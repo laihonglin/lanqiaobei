51a961eb9e2bbd7d5ee2bf02c51ae172c1d7b6e6
honglin <1300170430@qq.com>
2022-03-29 23:17:16 +0800

    最长回文子序列问题

---
+++ src/动态规划/最长回文子序列.java
@@ -0,0 +1,36 @@
+package 动态规划;
+
+/**
+ * 给你一个字符串 s ，找出其中最长的回文子序列，并返回该序列的长度。
+ * 子序列定义为：不改变剩余字符顺序的情况下，删除某些字符或者不删除任何字符形成的一个序列。
+ * <p>
+ * 示例:
+ * 输入：s = "bbbab"
+ * 输出：4
+ * 解释：一个可能的最长回文子序列为 "bbbb" 。
+ */
+public class 最长回文子序列 {
+    public static void main(String[] args) {
+        String s = "bbbab";
+        int res = longestPalindromeSubseq(s);
+        System.out.println(res);
+    }
+
+    private static int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 1;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = 2 + dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i + 1][j]);
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
