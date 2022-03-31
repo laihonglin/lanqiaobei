48e1c3f82d2bd797785e743807df64cf835d4258
honglin <1300170430@qq.com>
2022-03-31 23:12:09 +0800

    完全背包问题

---
+++ src/动态规划/完全背包问题.java
@@ -0,0 +1,41 @@
+package 动态规划;
+
+/**
+ * 给你一个整数数组 coins 表示不同面额的硬币，另给一个整数 amount 表示总金额。
+ * 请你计算并返回可以凑成总金额的硬币组合数。如果任何硬币组合都无法凑出总金额，返回 0 。
+ * <p>
+ * 示例：
+ * 输入：amount = 5, coins = [1, 2, 5]
+ * 输出：4
+ * 解释：有四种方式可以凑成总金额：
+ * 5=5
+ * 5=2+2+1
+ * 5=2+1+1+1
+ * 5=1+1+1+1+1
+ */
+public class 完全背包问题 {
+    public static void main(String[] args) {
+        int amount = 5;
+        int[] coins = {1, 2, 5};
+        int res = change(amount, coins);
+        System.out.println(res);
+    }
+
+    private static int change(int amount, int[] coins) {
+        int n = coins.length;
+        int[][] dp = new int[n + 1][amount + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (j - coins[i - 1] >= 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
+                } else {
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+        return dp[n][amount];
+    }
+}
