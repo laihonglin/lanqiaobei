92d2604d867512b9dcd7a148ec1d62b38d377c46
honglin <1300170430@qq.com>
2022-04-04 20:06:17 +0800

    区间调度问题

--- src/动态规划/区间调度问题.java
+++ src/动态规划/区间调度问题.java
@@ -1,83 +1,43 @@
 package 动态规划;
 
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.Comparator;
 
 /**
- * 有n项工作,每项工作分别在si时间开始,
- * 在ti时间结束.对于每项工作,你都可以选择参与与否.
- * 如果选择了参与,那么自始至终都必须全程参与.此外,
- * 参与工作的时间段不能重复(即使是开始的瞬间和结束的瞬间的重叠也是不允许的).
- * 你的目标是参与尽可能多的工作,那么最多能参与多少项工作呢?
- * <p>
- * 1≤n≤100000，1≤si≤ti≤109
- * <p>
- * 输入:
- * 第一行:n
- * 第二行:n个整数空格隔开,代表n个工作的开始时间
- * 第三行:n个整数空格隔开,代表n个工作的结束时间
- * <p>
- * 输出：常数（表示可参与的工作数）
- * <p>
- * 样例输入：
- * 5
- * 1 2 4 6 8
- * 3 5 7 9 10
- * <p>
- * 样例输出：
- * 3
+ * 给定一个区间的集合 intervals ，其中 intervals[i] = [start, end] 。
+ * 返回 需要移除区间的最小数量，使剩余区间互不重叠 。
+ * 示例：
+ * 输入: intervals = [[1,2],[2,3],[3,4],[1,3]]
+ * 输出: 1
+ * 解释: 移除 [1,3] 后，剩下的区间没有重叠。
+ * 相关例题: LeetCode 452 用最少数量的箭引爆气球
  */
 public class 区间调度问题 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] s = new int[n];
-        int[] t = new int[n];
-        Job[] jobs = new Job[n];
-        for (int i = 0; i < n; i++) {
-            s[i] = sc.nextInt();
-        }
-        for (int i = 0; i < n; i++) {
-            t[i] = sc.nextInt();
-        }
-        for (int i = 0; i < n; i++) {
-            jobs[i] = new Job(s[i], t[i]);
-        }
-        Arrays.sort(jobs);
-        int res = f(n, jobs);
+        int[][] intervals = {{1, 2}, {2, 3}, {3, 4}, {1, 3}};
+        int res = eraseOverlapIntervals(intervals);
         System.out.println(res);
     }
 
-    private static int f(int n, Job[] jobs) {
-        int cnt = 1;
-        int y = jobs[0].t;
-        for (int i = 0; i < n; i++) {
-            if (jobs[i].s > y) {
-                cnt++;
-                y = jobs[i].t;
+    private static int eraseOverlapIntervals(int[][] intervals) {
+        if (intervals.length == 0) {
+            return 0;
+        }
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+        int count = 1;
+        int x_end = intervals[0][1];
+        for (int[] interval : intervals) {
+            int start = interval[0];
+            if (start >= x_end) {
+                count++;
+                x_end = interval[1];
             }
         }
-        return cnt;
-    }
-
-    private static class Job implements Comparable<Job> {
-        int s;
-        int t;
-
-        public Job(int s, int t) {
-            this.s = s;
-            this.t = t;
-        }
-
-        //按结束时间从小到大排
-        //结束时间一样的，按开始时间从大到小排列
-        @Override
-        public int compareTo(Job other) {
-            int x = this.t - other.t;
-            if (x == 0) {
-                return this.s - other.s;
-            } else
-                return x;
-        }
+        return intervals.length - count;
     }
 }
