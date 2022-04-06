3ce36003792c58c1e2aae48bb4e0a7d819fc78e1
honglin <1300170430@qq.com>
2022-04-06 20:49:07 +0800

    最长递增子序列

---
+++ src/子序列/最长递增子序列.java
@@ -0,0 +1,38 @@
+package 子序列;
+
+import java.util.Arrays;
+
+/**
+ * 给你一个整数数组 nums ，找到其中最长严格递增子序列的长度。
+ * 子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。
+ * 例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
+ * <p>
+ * 示例：
+ * 输入：nums = [10,9,2,5,3,7,101,18]
+ * 输出：4
+ * 解释：最长递增子序列是 [2,3,7,101]，因此长度为 4 。
+ */
+public class 最长递增子序列 {
+    public static void main(String[] args) {
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        int res = lengthOfLIS(nums);
+        System.out.println(res);
+    }
+
+    private static int lengthOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        int res = 0;
+        for (int x : dp) {
+            res = Math.max(res, x);
+        }
+        return res;
+    }
+}
